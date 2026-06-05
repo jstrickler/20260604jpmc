717e1745d4e391a7dffe4cd5be9109908a2e61a7
jstrickler <jstrickler@gmail.com>
2026-06-05 11:44:12 -0400

    more dict examples

---
+++ EXAMPLES/dict_update.py
@@ -0,0 +1,24 @@
+spam = {
+    'a': 10,
+    'b': 20,
+    'c': 30,
+}
+
+ham = {
+    'c': 100, # will overwrite existing value
+    'd': 200,
+    'e': 300,
+}
+
+eggs = {
+    'e': 1000,
+    'f': 2000,
+    'g': 3000,
+}
+# new way as of v3.9
+spam |= ham
+print(f"{spam = }")
+
+# previous way (still works)
+spam.update(eggs)
+print(f"{spam = }")
